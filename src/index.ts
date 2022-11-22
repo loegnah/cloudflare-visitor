bc12c0520d65012570e161651fa8da9625a5336d
loegnah <loegnah@gmail.com>
2022-11-22 19:20:38 +0900

    Visitor count

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,37 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { makeBadge } from "./utils";
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
+  DB: KVNamespace;
+}
+
+async function fetchNav(request: Request, env: Env, ctx: ExecutionContext) {
+  const { pathname, searchParams } = new URL(request.url);
+  switch (pathname) {
+    case "/visit":
+      return handleVisit(searchParams, env.DB);
+    default:
+      return renderNotFound();
+  }
+}
+
+async function handleVisit(searchParams: URLSearchParams, db: KVNamespace) {
+  const visitorCnt = await db.get("visitorCnt");
+  const value = visitorCnt ? parseInt(visitorCnt) + 1 : 1;
+  await db.put("visitorCnt", value.toString());
+
+  return new Response(makeBadge(value), {
+    headers: {
+      "Content-Type": "image/svg+xml;",
+      "Cache-Control": "no-cache",
+      ETag: `"${Date.now().toString()}}"`,
+    },
+  });
+}
+
+function renderNotFound() {
+  return new Response(null, { status: 404 });
 }
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
+  fetch: fetchNav,
 };
